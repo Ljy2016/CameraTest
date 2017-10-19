3e1ec66ac85b2785511c790fd3218c43819b68d2
Ljy2016 <enjoy_azad@sina.com>
2017-10-19 18:07:22 +0800

    facedetect

--- app/src/main/java/com/example/azadljy/cameratest/CameraHelper.java
+++ app/src/main/java/com/example/azadljy/cameratest/CameraHelper.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.Surface;
 import android.widget.Toast;
 
+import java.util.List;
+
 /**
  * 作者：Ljy on 2017/10/16.
  * 功能：一些相机相关方法的封装
@@ -35,11 +37,6 @@ public class CameraHelper {
     }
 
 
-    public static void setParameters(Camera camera) {
-        Camera.Parameters parameters = camera.getParameters();
-        parameters.setPictureFormat(ImageFormat.JPEG);
-        camera.setParameters(parameters);
-    }
 
     //修正方向
     public static void setCameraDisplayOrientation(Activity activity,
@@ -97,4 +94,59 @@ public class CameraHelper {
         bmp.setPixels(rgba, 0, width, 0, 0, width, height);
         return bmp;
     }
+
+
+    /**
+     * Iterate over supported camera preview sizes to see which one best fits the
+     * dimensions of the given view while maintaining the aspect ratio. If none can,
+     * be lenient with the aspect ratio.
+     *
+     * @param sizes Supported camera preview sizes.
+     * @param w     The width of the view.
+     * @param h     The height of the view.
+     * @return Best match camera preview size to fit in the view.
+     */
+    public static Camera.Size getOptimalSize(List<Camera.Size> sizes, int w, int h) {
+        // Use a very small tolerance because we want an exact match.
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) w / h;
+        if (sizes == null)
+            return null;
+
+        Camera.Size optimalSize = null;
+
+        // Start with max value and refine as we iterate over available preview sizes. This is the
+        // minimum difference between view and camera height.
+        double minDiff = Double.MAX_VALUE;
+
+        // Target view height
+        int targetHeight = h;
+
+        // Try to find a preview size that matches aspect ratio and the target view size.
+        // Iterate over all available sizes and pick the largest size that can fit in the view and
+        // still maintain the aspect ratio.
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
+                continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        // Cannot find preview size that matches the aspect ratio, ignore the requirement
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+
 }
--- app/src/main/java/com/example/azadljy/cameratest/MainActivity.java
+++ app/src/main/java/com/example/azadljy/cameratest/MainActivity.java
@@ -28,6 +28,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.List;
 
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
@@ -65,11 +66,14 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         storage = opencv_core.CvMemStorage.create();
         File classifierFile = null;
         try {
+//            com.example.azadljy.cameratest
             classifierFile = Loader.extractResource(getClass(),
-                    "/org/bytedeco/javacv/facepreview/haarcascade_frontalface_alt.xml",
-                    context.getCacheDir(), "classifier", ".xml");
+                    "/assets/classifier/haarcascade_frontalface_alt2.xml",
+                    MainActivity.this.getExternalCacheDir(), "classifier", ".xml");
+            Log.e(TAG, "onCreate: " + context.getCacheDir());
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e(TAG, "onCreate:----------------- " + e.getMessage());
+//            e.printStackTrace();
         }
 //        if (classifierFile == null || classifierFile.length() <= 0) {
 //            throw new IOException("Could not extract the classifier file from Java resource.");
@@ -77,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
 
         // Preload the opencv_objdetect module to work around a known bug.
         Loader.load(opencv_objdetect.class);
+        Log.e(TAG, "onCreate: " + classifierFile.getAbsolutePath());
         classifier = new opencv_objdetect.CvHaarClassifierCascade(cvLoad(classifierFile.getAbsolutePath()));
     }
 
@@ -116,8 +121,8 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
     @AfterPermissionGranted(RC_CAMERA_PERM)
     public void getCamera() {
         if (hasCameraPermission()) {
-            camera = CameraHelper.openCamera(Camera.CameraInfo.CAMERA_FACING_BACK);
-            CameraHelper.setParameters(camera);
+            camera = CameraHelper.openCamera(Camera.CameraInfo.CAMERA_FACING_FRONT);
+            setParameters(1280, 720);
             CameraHelper.setCameraDisplayOrientation(this, Camera.CameraInfo.CAMERA_FACING_BACK, camera);
             SurfaceTexture texture = textureView.getSurfaceTexture();
             if (null != texture) {
@@ -140,38 +145,41 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         if (camera == null) {
             return;
         }
+
         try {
-            final byte[] bufferByte = new byte[textureView.getWidth() * textureView.getHeight() * 10];
+            final byte[] bufferByte = new byte[1280 * 720 * 3 / 2];
             Log.e(TAG, "startPreview: 字节数组的长度" + bufferByte.length);
+
             camera.addCallbackBuffer(bufferByte);
             camera.setPreviewCallbackWithBuffer(new Camera.PreviewCallback() {
                 @Override
                 public void onPreviewFrame(byte[] data, Camera camera) {
                     if (isStart) {
                         Log.e("TAG", "onPreviewFrame:开始 ");
+                        Log.e(TAG, "onPreviewFrame: " + textureView.getWidth() + textureView.getHeight());
+
                         isStart = false;
                         long startTime = System.currentTimeMillis();
                         Camera.Size size = camera.getParameters().getPreviewSize();
-//                        Bitmap bmp = CameraHelper.rawByteArray2RGBABitmap(data, size.width, size.height);
-//                        iv_bitmap_show.setImageBitmap(bmp);
-//                        long endTime = System.currentTimeMillis();
-//                        Log.e(TAG, "onPreviewFrame: 耗时" + (endTime - startTime));
-                        Log.e(TAG, "onPreviewFrame: " + size.height + "----" + size.width);
-                        Log.e(TAG, "onPreviewFrame: " + textureView.getWidth() + "----" + textureView.getHeight());
-                        try {
-                            YuvImage image = new YuvImage(data, ImageFormat.NV21, size.width, size.height, null);
-                            if (image != null) {
-                                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                                image.compressToJpeg(new Rect(0, 0, size.width, size.height), 100, stream);
-                                Bitmap bmp = BitmapFactory.decodeByteArray(stream.toByteArray(), 0, stream.size());
-                                iv_bitmap_show.setImageBitmap(bmp);
-                                //TODO：此处可以对位图进行处理，如显示，保存等
-                                long endTime = System.currentTimeMillis();
-                                Log.e(TAG, "onPreviewFrame: 耗时" + (endTime - startTime));
-                                stream.close();
+                        Log.e(TAG, "onPreviewFrame: " + size.width + size.height);
+                        if (processImage(data, size.width, size.height)) {
+                            try {
+                                YuvImage image = new YuvImage(data, ImageFormat.NV21, size.width, size.height, null);
+                                if (image != null) {
+                                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                                    image.compressToJpeg(new Rect(0, 0, size.width, size.height), 100, stream);
+                                    Bitmap bmp = BitmapFactory.decodeByteArray(stream.toByteArray(), 0, stream.size());
+                                    iv_bitmap_show.setImageBitmap(bmp);
+                                    //TODO：此处可以对位图进行处理，如显示，保存等
+                                    long endTime = System.currentTimeMillis();
+                                    Log.e(TAG, "onPreviewFrame: 耗时" + (endTime - startTime));
+                                    stream.close();
+                                }
+                            } catch (Exception ex) {
+                                Log.e("Sys", "Error:" + ex.getMessage());
                             }
-                        } catch (Exception ex) {
-                            Log.e("Sys", "Error:" + ex.getMessage());
+                        } else {
+                            Log.e(TAG, "onPreviewFrame: 没有检测到人脸");
                         }
                     }
                     camera.addCallbackBuffer(data);
@@ -190,7 +198,7 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         Log.e(TAG, "go: 点击了");
     }
 
-    protected void processImage(byte[] data, int width, int height) {
+    protected boolean processImage(byte[] data, int width, int height) {
         // First, downsample our image and convert it into a grayscale IplImage
         int f = SUBSAMPLING_FACTOR;
         if (grayImage == null || grayImage.width() != width / f || grayImage.height() != height / f) {
@@ -210,11 +218,39 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         }
 
         cvClearMemStorage(storage);
-        faces = cvHaarDetectObjects(grayImage, classifier, storage, 1.1, 3,
-                CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH);
-        // TODO: 2017/10/18      绘制边框
 
+        faces = cvHaarDetectObjects(grayImage, classifier, storage);
+
+        iv_bitmap_show.setImageBitmap(IplImageToBitmap(grayImage));
+        // TODO: 2017/10/18      绘制边框
+        if (faces != null) {
+            Log.e(TAG, "processImage: 人脸个数：" + faces.total());
+            if (faces.total() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
+    public void setParameters(int width, int height) {
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setPictureFormat(ImageFormat.JPEG);
+        List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
+        Camera.Size size = CameraHelper.getOptimalSize(previewSizes, width, height);
+        parameters.setPreviewSize(size.width, size.height);
+        Log.e("TAG", "setParameters: " + size.width + "---" + size.height);
 
+        camera.setParameters(parameters);
+    }
+
+    public Bitmap IplImageToBitmap(opencv_core.IplImage iplImage) {
+        Bitmap bitmap = null;
+        bitmap = Bitmap.createBitmap(iplImage.width(), iplImage.height(),
+                Bitmap.Config.ARGB_8888);
+        bitmap.copyPixelsFromBuffer(iplImage.getByteBuffer());
+        return bitmap;
+    }
 }
